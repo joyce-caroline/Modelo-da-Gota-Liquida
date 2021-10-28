2b6bcb5fd31724d84a29e70533ce496fc408bf8b
Joyce Caroline C. Amorim <58094660+joyce-caroline@users.noreply.github.com>
2021-10-27 23:54:31 -0300

    Upload

---
+++ modelo_gota_liquida.rb
@@ -0,0 +1,127 @@
+#Definição das funções para cálculo de massa
+class Mass 
+    def initialize
+        @a1 = 0.01691
+        @a2 = 0.01911
+        @a3 = 0.000763
+        @a4 = 0.10175
+        @a5 = 0.012
+    end
+
+    #Cálculo do número de neutrons
+    def setN (z, a)
+        @n = z - a 
+    end
+
+    #Definição da função principal
+    def dropModel (z, a)
+        value = f0(z, a) + f1(z, a) + f2(z, a) + f3(z, a) + f4(z, a) + f5(z, a)
+        return value
+    end
+
+    #Cálculo da massa das partes dos constituintes do átomo
+    def f0 (z, a)
+        f0 = 1.007825 * z + 1.008665 * (a - z)
+        return f0
+    end
+
+    #Termo de Volume
+    def f1 (z, a)
+        f1 = -@a1 * a
+        return f1
+    end
+
+    #Termo de Superfície
+    def f2 (z, a)
+        f2 =  @a2 * a**(2/3)
+        return f2
+    end
+
+    #Termo Coulombiano
+    def f3 (z, a)
+        f3 = + @a3 * z**2 / a**(1/3)
+        return f3
+    end
+
+    #Termo de Assimetria
+    def f4 (z, a)
+        f4 = + @a4 * (z - a/2)**2 / a
+        return f4 
+    end
+
+    #Termo de Emparelhamento
+    def f5 (z, a)
+        if z % 2 == 0 && @n % 2 == 0
+            f5 = - @a5 / Math.sqrt(a) 
+        elsif z % 2 == 0 && @n % 2 != 0
+            f5 = 0
+        elsif z % 2 != 0 && @n % 2 == 0
+            f5 = 0 
+        elsif z % 2 != 0 && @n % 2 != 0
+            f5 = - @a5 / Math.sqrt(a)
+        end
+        return f5
+    end
+
+    #Definição de função para cálculo da energia de ligação
+    def bindingEnergy (z, a)
+        return (f0(z, a) - dropModel(z, a)) * 931.5    
+    end
+
+    #Definição de função para cálculo da energia de ligação por nucleon
+    def nucleonEnergy (z, a)
+        return (f0(z, a) - dropModel(z, a)) / a * 931.5 
+        
+    end
+end 
+
+#Modelo nuclear da Gota Líquida
+
+var = Mass.new()
+
+loop do
+    puts "\t\t Modelo Nuclear da Gota Líquida"
+    print 'Digite o número de massa do átomo (A): '
+    a = gets.chomp.to_f
+    print "\nDigite o número atômico do átomo (Z): "
+    z = gets.chomp.to_f
+
+    var.setN(z, a)
+
+    puts "\n Escolha uma opção: "
+    puts '1 - Cálculo de massa.'
+    puts '2 - Cálculo da Energia de Ligação.'
+    puts '3 - Cálculo da Energia de Ligação por Nucleon'
+    puts '0 - Sair'
+
+    option = gets.chomp.to_i
+
+    case option
+        when 1
+            puts 'A massa nuclear desse átomo é:'
+            puts var.dropModel(z, a)
+            system "clear"
+
+        when 2
+            puts 'A energia de ligação desse átomo é:'
+            puts var.bindingEnergy(z, a)
+            system "clear"
+            
+        when 3
+            puts 'A energia de ligação por nucleon desse átomo é:'
+            puts var.nucleonEnergy(z, a)
+            system "clear"
+
+        when 0
+            puts 'Até Logo!'
+            system "clear"
+            break
+
+        else
+            puts 'Valor Inválido' 
+            system "clear"
+            
+        end
+end  
+     
+
